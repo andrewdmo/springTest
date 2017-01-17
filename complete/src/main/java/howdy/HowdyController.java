c0322567d916a57f5e67f42183cd669d2e9e800d
andrewdmo <andrewdmo@gmail.com>
2017-01-16 21:02:02 -0500

    -not working

---
+++ complete/src/main/java/howdy/HowdyController.java
@@ -0,0 +1,22 @@
+package howdy;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HowdyController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+    
+}
