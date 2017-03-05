764f30f46bc1d7b1914a6556c677a7ba007bc698
andrewdmo <andrewdmo@gmail.com>
2017-03-05 12:31:24 -0500

    -executable

--- src/main/java/com/herro/SecurityConfig.java
+++ src/main/java/com/herro/SecurityConfig.java
@@ -1,16 +1,16 @@
-package com.herro;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-            .withUser("user").password("password").roles("USER");
-    }
-}
+//package com.herro;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//            .inMemoryAuthentication()
+//            .withUser("user").password("password").roles("USER");
+//    }
+//}
--- src/main/java/com/herro/SecurityWebApplicationInitializer.java
+++ src/main/java/com/herro/SecurityWebApplicationInitializer.java
@@ -1,11 +1,11 @@
-package com.herro;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer
-    extends AbstractSecurityWebApplicationInitializer {
-
-    public SecurityWebApplicationInitializer() {
-        super(SecurityConfig.class);
-    }
-}
+//package com.herro;
+//
+//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//
+//public class SecurityWebApplicationInitializer
+//    extends AbstractSecurityWebApplicationInitializer {
+//
+//    public SecurityWebApplicationInitializer() {
+//        super(SecurityConfig.class);
+//    }
+//}
