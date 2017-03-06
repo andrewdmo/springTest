9bdbe1239f68207adef432a084d375732325a7df
andrewdmo <andrewdmo@gmail.com>
2017-03-06 17:50:09 -0500

    -ps included

--- src/main/java/com/herro/DualController.java
+++ src/main/java/com/herro/DualController.java
@@ -10,23 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
-//@EnableAutoConfiguration
 public class DualController {
 
     @RequestMapping(value = {"/", "/index"})
-    public String index(@RequestParam(value = "name", required = false, defaultValue = "Wirl") String name, Model
+    public String index(@RequestParam(value = "name", required = false, defaultValue = "Clint") String name, Model
         model) {
         model.addAttribute("name", name);
         return "index";
     }
 
     @RequestMapping("/mvcgreeting")
-    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "Wirl") String name, Model model) {
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "Clint") String name, Model model) {
         model.addAttribute("name", name);
         return "mvcgreeting";
     }
 
-    //    @RequestMapping("/error")
     @ExceptionHandler
     //add URL request query feedback later:
     public String error() {
@@ -37,11 +35,11 @@ public class DualController {
     @RestController
     public class restController {
 
-        private static final String template = "Herro, %s!";
+        private static final String template = "Stone Mountain greets you, %s!";
         private final AtomicLong counter = new AtomicLong();
 
         @RequestMapping("/restgreeting")
-        public Greeting restgreeting(@RequestParam(value = "name", defaultValue = "Wirl") String name) {
+        public Greeting restgreeting(@RequestParam(value = "name", defaultValue = "Clint") String name) {
             //ID increases by one every request:
             return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
