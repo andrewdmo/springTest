6a2fa0564dca790e09189070aad35b8178a4e826
andrewdmo <andrewdmo@gmail.com>
2017-07-11 12:46:59 -0400

    -local and DBs acting right for Users

--- src/main/java/com/herro/service/UserService.java
+++ src/main/java/com/herro/service/UserService.java
@@ -2,6 +2,7 @@ package com.herro.service;
 
 import com.herro.entity.User;
 import com.herro.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,7 +14,7 @@ public class UserService {
     private UserRepo userRepo;
 
     // AW breaks so turned off:
-    //    @Autowired
+    @Autowired
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
     }
