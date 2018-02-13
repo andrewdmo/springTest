e0bcc4f29e6b76186f165825470b2b931625a3de
andrewdmo <andrewdmo@gmail.com>
2018-02-13 11:37:14 -0500

    -live iframe map

--- toaws.sh
+++ toaws.sh
@@ -4,10 +4,10 @@ echo "Andy's first shell script"
 echo "which should pipe in to:"
 
 #change if/when needed
-CURRENTURL=ec2-user@ec2-52-206-81-235.compute-1.amazonaws.com
+CURRENTURL=ec2-user@ec2-54-156-212-170.compute-1.amazonaws.com
 
 echo $CURRENTURL
 
-ssh -v -i ~/.ssh/aws-eb ec2-user@ec2-52-206-81-235.compute-1.amazonaws.com
+ssh -v -i ~/.ssh/aws-eb ec2-user@ec2-54-156-212-170.compute-1.amazonaws.com
 
 echo "That just cost you money"
